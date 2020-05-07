3b31a9afb8545532ee70ecf78c345f96384486d0
Mustafa Aydemir <mustafa@aydemir.im>
2020-05-07 22:22:27 +0000

    Initial commit

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 [ -f /usr/local/bin/cc ] && sudo rm /usr/local/bin/cc;
-sudo curl -sSL https://raw.githubusercontent.com/criexe/dropbox-backup/master/main.sh -o /usr/local/bin/criexe-dropbox-backup && \
-sudo chmod a+x /usr/local/bin/criexe-dropbox-backup
+sudo curl -sSL https://raw.githubusercontent.com/mstfydmr/cc/master/cc.py -o /usr/local/bin/cc && \
+sudo chmod a+x /usr/local/bin/cc
